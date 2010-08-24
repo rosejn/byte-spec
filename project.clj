6dc7a9c84421f5b1b4d57d3a5a9884f81e7b3c8d
Jeff Rose <rosejn@gmail.com>
2010-08-24 16:46:04 +0200

    moving to clojure 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject overtone/byte-spec "0.0.2-SNAPSHOT"
+(defproject overtone/byte-spec "0.2.0-SNAPSHOT"
   :description "A DSL for reading and writing binary file formats in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]])
+                 [org.clojure/clojure-contrib "1.2.0"]])
